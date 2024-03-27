2cfefe4baea8e3208707c57f92a5d287cb248129
Kyle Saint Cloud <kyle@stcloud.house>
2024-03-27 17:34:42 -0500

    Middleware and Router tests

---
+++ middleware_test.go
@@ -0,0 +1,72 @@
+package main
+
+import(
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+// TestApiKeyNotPassed calls apiKeyRequired with a request that did not pass an API key.
+// If it is not passed we return a 401.
+func TestApiKeyNotPassed(t *testing.T) {
+
+  dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+  wrappedHandler := apiKeyRequired(dummyHandler)
+
+  req := httptest.NewRequest("GET", "/", nil)
+
+  rr := httptest.NewRecorder()
+
+  wrappedHandler.ServeHTTP(rr, req)
+
+  if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusUnauthorized)
+	}
+
+  parsed, err := decode[ApiErrorResponse](rr.Result())
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if success := parsed.Success; success != false {
+    t.Errorf("handler returned the wrong success: got %t want %t", success, false)
+  }
+
+  if errorMessage := parsed.Error; errorMessage != http.StatusText(http.StatusUnauthorized) {
+    t.Errorf("handler returned the wrong error: got %s want %s", errorMessage, http.StatusText(http.StatusUnauthorized))
+  }
+
+
+  if message := parsed.Message; message != apiKeyRequiredMessage {
+    t.Errorf("handler returned the wrong message: got %s want %s", message, apiKeyRequiredMessage)
+  }
+}
+
+// TestApiKeyPassed calls apiKeyRequired with a request that did pass an API key.
+// If it is passed we return a 200.
+func TestApiKeyPassed(t *testing.T) {
+
+  dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+  wrappedHandler := apiKeyRequired(dummyHandler)
+
+  req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+  req.Header.Add(apiKeyHeaderName, "anything")
+
+  rr := httptest.NewRecorder()
+
+  wrappedHandler.ServeHTTP(rr, req)
+
+  if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
---
+++ router_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TestHandleNotFound
+func TestHandleNotFound(t *testing.T) {
+  uri := "/api/nope"
+  req := httptest.NewRequest("GET", uri, nil)
+
+  req.Header.Add(apiKeyHeaderName, "anything")
+  rr := httptest.NewRecorder()
+  r := router()
+  r.ServeHTTP(rr, req)
+
+  if status := rr.Code; status != http.StatusNotFound {
+    t.Errorf("handler returned the wrong status code: got %v want %v", status, http.StatusNotFound)
+  }
+
+  parsed, err := decode[ApiErrorResponse](rr.Result())
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if success := parsed.Success; success != false {
+    t.Errorf("handler returned the wrong success: got %t want %t", success, false)
+  }
+
+  if errorMessage := parsed.Error; errorMessage != http.StatusText(http.StatusNotFound) {
+    t.Errorf("handler returned the wrong error: got %s want %s", errorMessage, http.StatusText(http.StatusNotFound))
+  }
+
+  expectedMessage := fmt.Sprintf("No endpoint at %s", uri)
+  if message := parsed.Message; message != expectedMessage {
+    t.Errorf("handler returned the wrong message: got %s want %s", message, expectedMessage)
+  }
+}
+
+func TestHandleMethodNotAllowed(t *testing.T) {
+  uri := "/api/weather"
+  method := "DELETE"
+  req := httptest.NewRequest(method, uri, nil)
+
+  req.Header.Add(apiKeyHeaderName, "anything")
+  rr := httptest.NewRecorder()
+  r := router()
+  r.ServeHTTP(rr, req)
+
+  if status := rr.Code; status != http.StatusMethodNotAllowed {
+    t.Errorf("handler returned the wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+  }
+
+  parsed, err := decode[ApiErrorResponse](rr.Result())
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if success := parsed.Success; success != false {
+    t.Errorf("handler returned the wrong success: got %t want %t", success, false)
+  }
+
+  if errorMessage := parsed.Error; errorMessage != http.StatusText(http.StatusMethodNotAllowed) {
+    t.Errorf("handler returned the wrong error: got %s want %s", errorMessage, http.StatusText(http.StatusNotFound))
+  }
+
+  expectedMessage := fmt.Sprintf("%s not allowed for endpoint", method)
+
+  if message := parsed.Message; message != expectedMessage {
+    t.Errorf("handler returned the wrong message: got %s want %s", message, expectedMessage)
+  }
+}
